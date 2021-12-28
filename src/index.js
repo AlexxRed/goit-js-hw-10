b09cd660d23f84b40b64791528d6891e991298d4
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-29 00:06:54 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+// ================== make imports ==================
 import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-
+// ================== var  ==================
 const DEBOUNCE_DELAY = 300;
 
+// ================== take ref  ==================
 const inputRef = document.querySelector('#search-box')
 console.log(inputRef);
 
+// ================== add listener  ==================
 inputRef.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 
-
+// ================== input change  ==================
 function onInput(e) {
     e.preventDefault();
     const inputValue = e.target.value.trim()
