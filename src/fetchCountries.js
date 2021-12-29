9cfe12838ab43276237c4e4b02d9221f1f95dbe6
Alexandr Krasnobokyi <krasnoboky@gmail.com>
2021-12-29 22:47:48 +0200

    sum

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,72 +1,92 @@
-import {countryListRef} from './index.js'
+import { countryListRef } from './index.js';
+import { countryInfoRef } from './index.js'
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 
 export function fetchCountries(name) {
     const responseFields = 'name,capital,population,flags,languages';
     fetch(`https://restcountries.com/v3.1/name/${name}?fields=${responseFields}`)
         .then(response => {
-        return response.json()
+            return response.json();
         
         })
         .then(
             countries => {
+                console.log(countries);
                 if (countries.length < 10) {
                     if (countries.length >= 2) {
                         countries.map(
                             (country => {
-                        const contryName = country.name.official
-                        const flagLink = country.flags.png
-                        console.log(contryName);
-                        console.log(flagLink);
-                        const htmlMarkup = `
+                                const contryName = country.name.official;
+                                const flagLink = country.flags.png;
+                        // console.log(contryName);
+                        // console.log(flagLink);
+
+                                const htmlMarkupCountry = `
                                 <li class="country-item">
                                 <img class="country-flag" src="${flagLink}" alt="">
                                 <p class="country-name">${contryName}</p>
                                 </li>
-                                `
-                        countryListRef.innerHTML += htmlMarkup;
-                        // countryListRef.insertAdjacentHTML("afterbegin", htmlMarkup)
+                                `;
+                                
+                        countryInfoRef.innerHTML += htmlMarkupCountry ;
                     })
                         )
                         
                     } else {
                         countries.map((country => {
-                        const contryName = country.name.official
-                        const capital = country.capital
-                        const population = country.population
-                        const flagLink = country.flags.png
-                        const languages = Object.values(country.languages)
-                        console.log(contryName);
-                        console.log(capital);
-                        console.log(population);
-                        console.log(flagLink);
-                            console.log(languages);
-                            const htmlMarkup = `
+                            const contryName = country.name.official;
+                            const capital = country.capital;
+                            const population = country.population;
+                            const flagLink = country.flags.png;
+                            const languages = Object.values(country.languages);
+                        // console.log(contryName);
+                        // console.log(capital);
+                        // console.log(population);
+                        // console.log(flagLink);
+                        // console.log(languages);
+
+                            const htmlMarkupCountries = `
                         <img class="flag" src="${flagLink}" alt="">
-                        <p class="name">${contryName}</p>
-                        <p class="capital">${capital}</p>
-                        <p class="population">${population}</p>
-                        <p class="languages">${languages}</p>`
-                        countryListRef.innerHTML = htmlMarkup;
+                        <p class="name"><b>Country:</b> ${contryName}</p>
+                        <p class="capital"><b>Capital:</b> ${capital}</p>
+                        <p class="population"><b>Population:</b> ${population}</p>
+                        <p class="languages"><b>Languages:</b> ${languages}</p>`;
+                            
+                        countryListRef.innerHTML = htmlMarkupCountries;
             }))
                     }
                     
                 } else {
-                    console.log("Too many matches found. Please enter a more specific name." );
-                }
-            console.log(countries);
+                    if (countries.message ==='Not Found') {
+                        Notify.failure("Oops, there is no country with that name");
+                    } else {
+                        Notify.info("Too many matches found. Please enter a more specific name." );
+                    console.log("Too many matches found. Please enter a more specific name.");
+                    };
+                };
+            // console.log(countries);
 
             
         })  
         .catch(error => {
-            console.log(error)
-            alert("Oops, there is no country with that name")
+            console.log(error);
+            Notify.failure("Oops, there is no country with that name");
+            console.log("Oops, there is no country with that name");
         });
 };  
 
 
 
-
-
+// function htmlMarkupCountries({ flagLink, contryName, capital, population, languages }) {
+//     `
+// <img class="flag" src="${flagLink}" alt="">
+// <p class="name">${contryName}</p>
+// <p class="capital">${capital}</p>
+// <p class="population">${population}</p>
+// <p class="languages">${languages}</p>
+// `;
+// }
 
 
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,29 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+// ================== Notiflix  init ==================
+
+Notify.init({
+width: '700px',
+position: 'right-top',
+closeButton: false,
+distance: '10px',
+opacity: 5,
+borderRadius: '5px',
+rtl: false,
+timeout: 2000,
+messageMaxLength: 110,
+backOverlay: false,
+backOverlayColor: 'rgba(0,0,0,0.9)',
+plainText: true,
+showOnlyTheLastOne: false,
+clickToClose: true,
+pauseOnHover: true,
+zindex: 4001,
+fontFamily: 'Quicksand',
+fontSize: '32px',
+});
+
 // ================== var  ==================
 const DEBOUNCE_DELAY = 300;
 
@@ -12,6 +35,8 @@ const inputRef = document.querySelector('#search-box')
 export const countryListRef = document.querySelector('.country-list')
 export const countryInfoRef = document.querySelector('.country-info')
 // console.log(inputRef);
+
+
 
 // ================== add listener  ==================
 inputRef.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
@@ -23,10 +48,14 @@ function onInput(e) {
     const inputValue = e.target.value.trim()
     console.log(inputValue);
     if (inputValue === '') {
-        console.log('no symbol - clear country list');
+        countryListRef.innerHTML = ''
+        countryInfoRef.innerHTML = ''
+        Notify.info('Start typing the country name');
+        console.log('Start typing the country name');
         return
     } else {
         countryListRef.innerHTML = ''
+        countryInfoRef.innerHTML = ''
         fetchCountries(inputValue);
     }
 
